11db29b1abedd307e3160cb30857f112c471ea8a
one-tokyo <info@one.tokyo.jp>
2022-06-09 10:59:23 +0900

    0609_1100

--- dist/_nuxt/static/1654735096/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontact","\u002Farchives","\u002Fabout","\u002F","\u002Farchives\u002Fshochux","\u002Farchives\u002Fone","\u002Farchives\u002Fthalassic","\u002Farchives\u002Flamie","\u002Farchives\u002Fjiunu","\u002Farchives\u002Falebe","\u002Farchives\u002Fmakina"]})
